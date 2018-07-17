bb5f4a736037c4abd9d9896c1db369daafaee3fc
WinstonKamau <crispus.kamau@andela.com>
2018-07-17 14:47:46 +0300

    test

---
+++ my-app/src/components/homePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    render() {
+        return(
+            <div className="jumbotron">
+                <h1>My Deeper Study Case of React js</h1>
+                <p>Learning best practices for react</p>
+            </div>
+        );
+    }
+};
